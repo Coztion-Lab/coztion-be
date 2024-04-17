59ca935714ff27ecae679bbf3726e2987a38c7bd
codesver <codesver@gmail.com>
2024-04-17 22:30:23 +0900

    init

---
+++ src/main/java/codesver/coztion/CoztionApplication.java
@@ -0,0 +1,11 @@
+package codesver.coztion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoztionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoztionApplication.class, args);
+    }
+}
